35ef56d6c129e0e2ea8e80caa20e0bb731fed795
joekendal <13680617+joekendal@users.noreply.github.com>
2022-01-10 21:38:25 +0000

    feat: additional interface methods

--- consult/consultiface/interface.go
+++ consult/consultiface/interface.go
@@ -5,7 +5,18 @@ import (
 )
 
 type ConsultAPI interface {
+	// Get a questionnaire by Id
+	GetQuestionnaire(id string) (*consult.GetQuestionnaire, error)
+
+	// Create a new consultation
+	CreateConsultation(consult.CreateConsultationInput) (*consult.CreateConsultation, error)
+
+	// Create a new questionnaire
+	CreateQuestionnaire(consult.CreateQuestionnaireInput) (*consult.CreateQuestionnaire, error)
+
+	// Answer an existing questionnaire
 	AnswerQuestionnaire(consult.AnswerQuestionnaireInput) (*consult.AnswerQuestionnaire, error)
 
+	// Set the Authorization header
 	SetApiKey(key string)
 }
