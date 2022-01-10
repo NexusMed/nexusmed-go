e980f300439c7b44589e2ba6b9526a77bb16d435
J Kendal <13680617+joekendal@users.noreply.github.com>
2022-01-10 01:49:30 +0000

    Update util.go

--- util.go
+++ util.go
@@ -3,7 +3,7 @@ package nexusmed
 import "github.com/nexusmed/nexusmed-go/consult"
 
 // Converts []consult.AnswerInput to []*consult.AnswerInput
-func Answers(in []consult.AnswerInput) []*consult.AnswerInput {
+func AnswersInput(in []consult.AnswerInput) []*consult.AnswerInput {
 	out := make([]*consult.AnswerInput, len(in))
 	for i := range in {
 		out[i] = &in[i]
