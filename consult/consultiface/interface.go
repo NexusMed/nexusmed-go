e5450920b8f739e0fc0ff692f032e7cd8bfcf7ef
joekendal <13680617+joekendal@users.noreply.github.com>
2022-01-10 03:07:20 +0000

    fix: interface

--- consult/consultiface/interface.go
+++ consult/consultiface/interface.go
@@ -1,13 +1,12 @@
 package consultiface
 
 import (
-	"github.com/nexusmed/nexusmed-go/client"
 	"github.com/nexusmed/nexusmed-go/consult"
 )
 
 type ConsultAPI interface {
 	AnswerQuestionnaire(consult.AnswerQuestionnaireInput) (*consult.AnswerQuestionnaire, error)
-	
-	New() *Client
+
+	New() *consult.Client
 	SetApiKey(key string)
 }
