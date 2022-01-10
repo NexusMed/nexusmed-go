3dd0219d09a37841ca1fa6c4fcec4c391931d34f
joekendal <13680617+joekendal@users.noreply.github.com>
2022-01-10 01:46:19 +0000

    feat: util for answers pointer conversion

---
+++ util.go
@@ -0,0 +1,12 @@
+package nexusmed
+
+import "github.com/nexusmed/nexusmed-go/consult"
+
+// Converts []consult.AnswerInput to []*consult.AnswerInput
+func Answers(in []consult.AnswerInput) []*consult.AnswerInput {
+	out := make([]*consult.AnswerInput, len(in))
+	for i := range in {
+		out[i] = &in[i]
+	}
+	return out
+}
